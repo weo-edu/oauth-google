d17e1f5196b6a138762ceda6531b8909a93b96c9
Andrew Shaffer <andrew@weo.io>
2015-03-13 11:43:13 -0700

    s/scopes/scope

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-module.exports = function(clientId) {
+module.exports = function(opts) {
   var provider = {
     baseUrl: 'https://accounts.google.com/o/oauth2/auth',
     scopeDelimiter: ' ',
     scopePrefix: 'openid',
     scope: ['profile', 'email'],
-    clientId: clientId,
     display: 'popup',
     popupDefaults: {
       width: 452,
       height: 633
     }
   };
+
+  Object.keys(opts).forEach(function(key) {
+    provider[key] = opts[key];
+  });
 
   return function(oauth) {
     oauth.provider = provider;
